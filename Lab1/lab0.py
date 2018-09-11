c0ffb5cda718cbf14ef305b1e093f10dfeecab0f
Marius Longeanie <WTSTUDENTS\ml1020093@ecsl-21.wtstudents.wtamu.edu>
2018-09-11 10:59:58 -0500

    Lab1 introduction to python

---
+++ Lab1/lab0.py
@@ -0,0 +1,20 @@
+Python 3.5.2 (default, Nov 23 2017, 16:37:01) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello out there !")
+Hello out there !
+>>> 8+5
+13
+>>> (8+2)*3
+30
+>>> x=5
+>>> x+10
+15
+>>> print(x)
+5
+>>> X+5
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    X+5
+NameError: name 'X' is not defined
+>>> 
